65ec10cbfa8dd3ad3d9d9299b8e262631a0ed1b6
panguido <i.tico@hotmail.com>
2017-10-08 17:57:45 -0600

    logo
    principal y logo

--- login.php
+++ login.php
@@ -2,12 +2,54 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>login</title>
+	<title> barberia Morera</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/login.css">
+	<link rel="stylesheet" href="css/registro.css">
 </head>
 <body>
 
+
+
+
+<div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+			    		<h3 class="panel-title">Bienvenido a su Barberia..!  <small>Es gratuita!</small></h3>
+			 			</div>
+			 			<div class="panel-body">
+			    		<form role="form">
+			    			
+
+			    			<div class="form-group">
+			    				<input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email...">
+			    			</div>
+
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password" id="password" class="form-control input-sm" placeholder="contrasenna...">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Confirm contrasenna">
+			    					</div>
+			    				</div>
+			    				  
+			    			</div>
+			    			
+			    			<input type="submit" value="Register" class="btn btn-info btn-block">
+			    		<br>
+                        <input type="reset">
+                         <input type="submit">
+			    		</form>
+			    	</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>	
 </body>
---
+++ registro.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro de usuario</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+
+
+<div class="container">
+        <div class="row">
+
+            <div class="col-md-8 col-md-offset-2">
+                <form role="form" method="POST" action="#">
+
+                    <legend class="text-center">Registro</legend>
+
+                    <fieldset>
+                        <legend> Detalles de la Cuenta</legend>
+
+                        <div class="form-group col-md-6">
+                            <label for="first_name">First name</label>
+                            <input type="text" class="form-control" name="" id="first_name" placeholder="First Name">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="last_name">Last name</label>
+                            <input type="text" class="form-control" name="last_name" id="" placeholder="Last Name">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="">Email</label>
+                            <input type="email" class="form-control" name="" id="" placeholder="Email">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="" id="password" placeholder="Password">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="confirm_password">Confirm Password</label>
+                            <input type="password" class="form-control" name="" id="confirm_password" placeholder="Confirm Password">
+                        </div>
+
+
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Detalles opcionales de informacion..!!</legend>
+
+                        <div class="form-group col-md-6">
+                            <label for="country"> Residencia </label>
+                            <select class="form-control" name="" id="country">
+                                <option>Alajuela 1</option>
+                                <option>San jose 2</option>
+                                <option>Heredia 3</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="found_site">¿Como se entero o informo sobre nosotros..?</label>
+                            <select class="form-control" name="" id="found_site">
+                                <option>Company</option>
+                                <option>Friend</option>
+                                <option>Colleague</option>
+                                <option>Advertisement</option>
+                                <option>Google Search</option>
+                                <option>Online Article</option>
+                                <option value="other" >Other</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-md-12 hidden">
+                            <label for="specify">porfavor informenos</label>
+                            <textarea class="form-control" id="specify" name=""></textarea>
+                        </div>
+
+                    </fieldset>
+
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" value="" id="">s
+                                    I accept the <a href="#">terms and conditions</a>.
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-primary">
+                                Registro..!
+                            </button>
+                            <a href="#">Already have an account?</a>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>	
+</body>
+</html>
