3f52f324021672eaa033a43db18aa0ad0f03a27a
panguido <i.tico@hotmail.com>
2017-10-08 16:45:12 -0600

    nuevo codigo
    creación de login

---
+++ css/test.php
@@ -0,0 +1,41 @@
+//arrays
+<?php
+$a = array();
+$a[]= "uno";
+$b[]= "dos";
+$c["tres"]= "tres"
+$a[]=cuatro;
+
+print_r($a");
+
+
+?>
+<?php
+ $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+$nombre = "Aner"; 
+
+ 
+//saco el numero de elementos
+$longitud = count($ceu);
+ 
+//Recorro todos los elementos
+for($i=0; $i<$longitud; $i++)
+      {
+      //saco el valor de cada elemento
+	  echo $ceu[$i];
+	  echo "<br>";
+      }
+?>
+<?php
+ 
+
+$a = array2(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79);
+echo "sum(a) = " . array_sum($a) . "\n";
+ 
+$suma = array_sum($array2);
+$total_numeros = count($array2);
+$media = $suma/$total_numeros;
+$media = array_sum($array2)/count($array2);
+
+
+ ?>
---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>login</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>	
+</body>
+</html>
