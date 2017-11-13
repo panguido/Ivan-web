a24d74c9a19eb601509eafc5c6004cc504ce5427
panguido <i.tico@hotmail.com>
2017-11-12 22:05:50 -0600

    mantenimientos
    agregar paginas de mantenim usuarios

--- js/registro.js
+++ js/registro.js
@@ -8,40 +8,19 @@
           mapTypeId: 'roadmap'
         });
 
-        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-        var icons = {
-          parking: {
-            icon: iconBase + "../barberia/barP.png"
-          },
-          library: {
-            icon: iconBase + 'library_maps.png'
-          },
-          info: {
-            icon: iconBase +  'info-i_maps.png'
-          }
-        };
-
-        var features = [
-          {
-            position: new google.maps.LatLng(9.748916999999999 ,-83.75342799999999 ), 
-            type: 'info'
-          }, {
-            position: new google.maps.LatLng(10.471755,-84.641236), 
-            type: ' parking'
-          }, {
-            position: new google.maps.LatLng( 10.622248,-84.528004),
-            type: 'library'
-          }, {
-            position: new google.maps.LatLng(10.375248,-84.341472),
-            type: 'info'
-          }
-            
-        ];
-        var marker = new google.maps.Marker({
-    position: myLatLng,
+        var image = 'barberia/vector.png';
+  var beachMarker = new google.maps.Marker({
+    position: {lat: 10.471755, lng: -84.641236},
     map: map,
-    title: 'barberia/barP.png'
+    icon: image
   });
+   var image = 'barberia/vector.png';
+  var beachMarker = new google.maps.Marker({
+    position: {lat:10.6222489, lng: -84.528004},
+    map: map,
+    icon: image
+  });
+        
 
         // Create markers.
         features.forEach(function(feature) {
