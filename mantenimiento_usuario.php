89a331a290c8bfe674940ee49ae1c5e21e195926
panguido <i.tico@hotmail.com>
2017-12-10 01:09:47 -0600

    registro tablas
    registros tablas

--- mantenimiento_usuario.php
+++ mantenimiento_usuario.php
@@ -110,13 +110,9 @@ if(!$query=$con->query($sql)){
                                                 <td>
                                                     <p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" name="editar" type="button" data-target="#edit" onclick="editarRutina(this.value)" value="<?php echo $idT; ?>"><span class="glyphicon glyphicon-pencil"></span></button></p>
                                                 </td>
-                                                <td>
-                                                    <p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" name="editar" type="button" data-target="#edit" onclick="editarMedida(this.value)" value="<?php echo $idT; ?>"><span class="glyphicon glyphicon-pencil"></span></button></p>
-                                                </td>
-                                                <td>
-                                                    <p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" name="editar" type="button" data-target="#edit" onclick="editarComida(this.value)" value="<?php echo $idT; ?>"><span class="glyphicon glyphicon-pencil"></span></button></p>
-                                                </td>
-                                            </tr>
+                                             
+                                              <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+                                                </tr>
                                             <?php
                                         }
                                         if ($contador === 0) {
